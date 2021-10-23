3dd058ae48e004e8264d8eb846fa7a544064ca59
ahasa <ahasa@DESKTOP-A8O5G04.station>
2021-10-23 13:41:27 +0100

    hi

---
+++ Practice_Maven_Cocumber_With_Reports_2/src/test/java/runner/TestRunner.java
@@ -0,0 +1,28 @@
+package runner;
+
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/resources/features",
+glue= {"steps"}
+//Create an HTML Report By Adding A Plugin To Testrunner.Java Class
+//plugin = { "pretty", "html:target/htmlreports" }
+
+//Create HTML Report By Using Extent-Reports. Add plugin in POM file.
+//plugin = {"json:target/cucumber.json"}
+)
+
+public class TestRunner {
+	//Create HTML Report By Using Extent-Reports. 2/2
+//    @AfterClass
+//    public static void writeExtentReport() {
+//        Reporter.loadXMLConfig(new File("config/report.xml"));
+}
